3a7ed2d61af8a5596bfe64726e7e9d472f956e00
= <iw3@calvin.edu>
2024-11-19 19:46:17 -0500

    update_db

--- sql/CalvinKlean_db.sql
+++ sql/CalvinKlean_db.sql
@@ -1,21 +1,12 @@
 -- Drop previous versions of the tables if they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS MachineLocation CASCADE;
 DROP TABLE IF EXISTS Machine CASCADE;
-DROP TABLE IF EXISTS Users CASCADE;
 DROP TABLE IF EXISTS Dorm CASCADE;
 
 -- Create the Dorm table.
 CREATE TABLE Dorm (
     ID integer PRIMARY KEY,
     name varchar(50) NOT NULL
-);
-
--- Create the Users table, referencing Dorm.
-CREATE TABLE Users (
-    ID integer PRIMARY KEY,
-    dormID integer REFERENCES Dorm(ID),
-    name varchar(50) NOT NULL,
-    emailAddress varchar(50) NOT NULL
 );
 
 -- Create the Machine table.
@@ -35,9 +26,6 @@ CREATE TABLE MachineLocation (
 -- Insert statements for initial data
 INSERT INTO Dorm (ID, name) VALUES (1, 'Vanderwerp');
 INSERT INTO Dorm (ID, name) VALUES (2, 'Noordewier');
-
-INSERT INTO Users (ID, dormID, name, emailAddress) VALUES (1, 1, 'John Doe', 'john@example.com');
-INSERT INTO Users (ID, dormID, name, emailAddress) VALUES (2, 2, 'Jane Smith', 'jane@example.com');
 
 INSERT INTO Machine (ID, type, availability) VALUES (1, 'washer', true);
 INSERT INTO Machine (ID, type, availability) VALUES (2, 'dryer', true);
